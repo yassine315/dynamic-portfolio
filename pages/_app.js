55722a2999014eb2620f4d292cb33b43d002124a
YASSINE EL-MAHI <yassine.elmahi@keiken-digital.com>
2021-12-24 16:08:19 +0100

    init repo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { parse } from 'node-html-parser';
 
 function MyApp({ Component, pageProps }) {
+  scrap()
   return <Component {...pageProps} />
 }
+
+function scrap(){
+  if(typeof window === 'undefined'){
+    fetch(`https://www.credly.com/users/yassine-el-mahi`).then((value)=>{
+      value.text().then((text)=>{
+        const root = parse(text);
+        const badges = root.querySelector(".data-table-grid__rows").childNodes
+
+        console.log(badges)
+      })
+    })
+  }
+}
+
 
 export default MyApp
